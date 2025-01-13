826bb4a64fdb03d170b1de733025cc58caa62360
Joseph MacMillan <joseph.macmillan@ontariotechu.ca>
2025-01-13 14:32:13 -0500

    clean up

--- 1-4 Solving ODEs/euler.py
+++ 1-4 Solving ODEs/euler.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from phy4910 import ode_euler
 
 # solve the ode via euler
 
@@ -9,22 +10,7 @@ def f(x, y, z):
 def g(x, y, z):
 	return -1/y * z**2
 
-h = 0.01
-x = np.arange(0, 10, h)
-N = len(x)
+x, y, z = ode_euler(0, 10, 0.001, 1, 1, f, g)
 
-y = np.zeros(N)
-z = np.zeros(N)
-
-# set BCs
-y[0] = 1
-z[0] = 1
-
-for i in range(N-1):
-	k1 = h * f(x[i], y[i], z[i])
-	ell1 = h * g(x[i], y[i], z[i])
-	
-	y[i+1] = y[i] + k1
-	z[i+1] = z[i] + ell1
 
 np.savetxt("euler_data.txt", np.column_stack((x,y,z)))
